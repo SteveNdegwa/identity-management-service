c3d14698f61c4c5133df8a9c226c850e25bc7d18
stevendegwa <stevencallistus19@gmail.com>
2026-05-28 08:35:14 +0300

    update cors allowed origins

--- spin_identity/settings.py
+++ spin_identity/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'spin_identity.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    'https://stage-accounts.spinmobile.co',
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
